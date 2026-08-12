1962747d17dbd60630cfb7a0889b3c3a7880f9f7
Genius <53309945+i-am-genius@users.noreply.github.com>
2026-08-12 05:59:46 -0700

    fix: import garment calibration minimum constraint

--- src/main/java/com/genius/smartlight/vo/device/DeviceGarmentAimCalibrationSampleReqVO.java
+++ src/main/java/com/genius/smartlight/vo/device/DeviceGarmentAimCalibrationSampleReqVO.java
@@ -2,6 +2,7 @@ package com.genius.smartlight.vo.device;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 

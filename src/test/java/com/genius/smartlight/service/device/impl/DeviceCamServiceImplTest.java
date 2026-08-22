f0adabce0735b6f65e1c737c07d578b32ce444da
Genius <53309945+i-am-genius@users.noreply.github.com>
2026-08-22 20:57:42 +0800

    test: import Mockito atLeastOnce matcher

--- src/test/java/com/genius/smartlight/service/device/impl/DeviceCamServiceImplTest.java
+++ src/test/java/com/genius/smartlight/service/device/impl/DeviceCamServiceImplTest.java
@@ -55,6 +55,7 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyBoolean;
 import static org.mockito.ArgumentMatchers.argThat;
 import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.atLeastOnce;
 import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.inOrder;
 import static org.mockito.Mockito.mock;
